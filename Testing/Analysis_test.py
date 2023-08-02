6be10b1897d3fa588885e9a2dc091d1af91d8e2f
RossAmory <pcyra2@nottingham.ac.uk>
2023-08-02 09:56:24 +0100

    Added wham correlation test

---
+++ Testing/Analysis_test.py
@@ -0,0 +1,8 @@
+import pyBrellaSampling.analysis as analysis
+
+test_bond = analysis.BondClass(1,2,"testbond",
+                               1.2)
+test_dihedral = analysis.DihedralClass(1,2,3,4,
+                                       "Test Dihedral",0,"Min",
+                                       180,"Max")
+
