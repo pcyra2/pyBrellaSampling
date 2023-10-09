397faa1e9d3f5ae696aa01dc322eef7941019476
pcyra2 <pcyra2@nottingham.ac.uk>
2023-10-09 11:13:43 +0100

    Added setup.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+# set up using "pip install -e ."
+setup(
+    name='pyBrellaSampling',
+    version='1.0',
+    py_modules=['pyBrellaSampling'],
+    entry_points={
+        'console_scripts': [
+            'pyBrella = pyBrellaSampling.pyBrella:main',
+        ],
+    },
+)
