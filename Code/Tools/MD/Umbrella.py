6b8ba64666c7438ebbc5f8155c1ff7db62ff07a8
Ross <ross.amory98@gmail.com>
2025-04-03 16:35:18 +0100

    added finished check

--- Code/Tools/MD/Umbrella.py
+++ Code/Tools/MD/Umbrella.py
@@ -264,9 +264,11 @@ forceConstant   {self.colvar.HoldForce}
                 io.textDump(slurmscript, os.path.join(WorkDir, f"sub-Umbrella-{job["output"]}.sh"))
                 io.textDump(HPC.arrayjobscript, os.path.join(WorkDir, "array_job.sh"))
                 if job["run"] == "true":
-                    status = HPC.check_dependecy(job["output"])
-                    if status != "wait":
-                        HPC.run_slurmScript(os.path.join(WorkDir, f"sub-Umbrella-{job["output"]}.sh"))
+                    filepath = os.path.join(WorkDir, str(i), f"{job["output"]}")
+                    if MM.software.check_output(f"{filepath}.out")[0] != "completed":
+                        status = HPC.check_dependecy(job["output"])
+                        if status != "wait":
+                            HPC.run_slurmScript(os.path.join(WorkDir, f"sub-Umbrella-{job["output"]}.sh"))
         else:
             for i in range(NPartitions):
                 if HPC.exists:
@@ -290,6 +292,8 @@ forceConstant   {self.colvar.HoldForce}
                     io.textDump(slurmscript, os.path.join(WorkDir, f"sub-Umbrella-{job["output"]}_{i+1}.sh"))
                     io.textDump(HPC.arrayjobscript, os.path.join(WorkDir, "array_job.sh"))
                     if job["run"] == "true":
-                        status = HPC.check_dependecy(f"{job["output"]}_{i+1}")
-                        if status != "wait":
-                            HPC.run_slurmScript(os.path.join(WorkDir, f"sub-Umbrella-{job["output"]}_{i+1}.sh"))
+                        filepath = os.path.join(WorkDir, str(i), f"{job["output"]}")
+                        if MM.software.check_output(f"{filepath}.out")[0] != "completed":
+                            status = HPC.check_dependecy(f"{job["output"]}_{i+1}")
+                            if status != "wait":
+                                HPC.run_slurmScript(os.path.join(WorkDir, f"sub-Umbrella-{job["output"]}_{i+1}.sh"))
